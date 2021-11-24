d1e645cc2389378394eb890fed3537b0d0a7c922
jimao <jimao@jimaodeMacBook-Pro.local>
2021-11-24 11:00:32 +0800

    基本方法

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,37 @@
 
-console.log('你好，世界');
+export
+class values {
+  public constructor(
+    private nums: number[],
+  ) { }
+
+  private normalizeEndStart(
+    end?: number,
+    start?: number,
+  ) {
+    return [
+      end != null ? end : this.nums.length,
+      start != null ? start : 0,
+    ];
+  }
+
+  public slice(end?: number, start?: number) {
+    const [iend, istart] = this.normalizeEndStart(end, start);
+    return this.nums.slice(istart, iend);
+  }
+
+  public min(end?: number, start?: number) {
+    return Math.min(...this.slice(end, start));
+  }
+
+  public max(end?: number, start?: number) {
+    return Math.max(...this.slice(end, start));
+  }
+
+  public avg(end?: number, start?: number) {
+    const slice = this.slice(end, start);
+    let sum = 0;
+    slice.forEach((num) => sum += num);
+    return sum / slice.length;
+  }
+}
